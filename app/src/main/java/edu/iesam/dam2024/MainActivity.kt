b46743b5aaf6c62f88c8bc027b3de9a2c9f8db11
PolRubiano <diego@pigames.es>
2024-09-18 17:06:50 +0200

    240918

--- app/src/main/java/edu/iesam/dam2024/MainActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/MainActivity.kt
@@ -10,15 +10,34 @@ class MainActivity : AppCompatActivity() {
 
     // private val varInmutable: String = "Hola"
     // private var varMutable: String = "Hola"
+
+    private var name: String = "Diego"
+    private var name2: String? = null // le decimos que la variable puede ser nula con el ? en el tipo
+    private var name3: String = null // si no se lo marcamos (?) nos avisa de error
+
+    // inicia los contructores
+    // init {
+    //     name = "Diego"
+    // }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main) // R son recursos en Android
+
+        //Scope Function
+        // si el name no es nulo ejecuta el codigo
+        name2?.let { nameNotNull ->
+            Log.d("@dev", nameNotNull)
         }
+
+        // si no es nulo (it) hace referencia a name2
+        name2 = "Alumno"
+        name2?.let {
+            Log.d("@dev", it)
+        }
+
+        // fuerza a imprimir el contenido de la variable, se nulo o no, si es nulo la app crasheará
+        Log.d("@dev", name3!!)
+
     }
 }
